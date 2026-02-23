1805ed76f86e6afb9832e785462cec1b243ff4d3
PeterHUistyping <2447276617@qq.com>
2026-02-23 15:03:08 +0000

    tried macOS app.

--- scrcpy_install.sh
+++ scrcpy_install.sh
@@ -2,3 +2,11 @@ brew install scrcpy
 brew install --cask android-platform-tools
 
 adb devices
+
+# commit as a macOS app via pyinstaller
+pip install pyinstaller
+# --windowed: no console, only the app window
+pyinstaller --onefile \
+  --add-data "archive/YanYun.json:archive" \
+  keyboard-map.py
+pyinstaller main.spec
